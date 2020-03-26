7be81034417fce5494a5107d076fed3cf7bdb266
lkaraba <62667606+lkaraba@users.noreply.github.com>
2020-03-26 17:18:08 -0400

    Update initialPull.m
    Added lines to import NPY data, set up initial plotting

--- initialPull.m
+++ initialPull.m
@@ -1,7 +1,13 @@
 clc
 clear
 
-amplitude = readNPYheader(amplitudes.npy);
-%{
+%%Import relevant data
+amp = readNPY('C:\Users\karab\Documents\GitHub\BME410-DataAnalysis\amplitudes.npy'); %Spike amplitude
+time = readNPY('C:\Users\karab\Documents\GitHub\BME410-DataAnalysis\spike_times.npy'); %Spike times
+spiketemp = readNPY('C:\Users\karab\Documents\GitHub\BME410-DataAnalysis\spike_templates.npy');
+chanMap = readNPY('C:\Users\karab\Documents\GitHub\BME410-DataAnalysis\channel_map.npy');%sorts channel with cluster
+cluster = readNPY('C:\Users\karab\Documents\GitHub\BME410-DataAnalysis\spike_clusters.npy');%spikes per cluster?
 
-}%
+%%Plotting
+figure(1)
+plot(time(1:1000), amp(1:1000));
