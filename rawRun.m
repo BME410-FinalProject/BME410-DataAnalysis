34feac588f77ee2513fd8cfbdf9170471a0a3529
Lindsay Karaba <karaba24@gmail.com>
2020-03-31 19:10:17 -0400

    Update rawRun.m
    Plots raw data traces

--- rawRun.m
+++ rawRun.m
@@ -1,7 +1,19 @@
+addpath('D:\Documents\Word\College\Junior Year\Semester 2\BME 410\npy-matlab-master')
+savepath
+
 fn = 'D:\Documents\Word\College\Junior Year\Semester 2\BME 410\rawDataSample.bin';
 fid = fopen(fn, 'r');
-dat = fread(fid, [385 Inf], '*int16');
+rawData = fread(fid, [385 Inf], '*int16');
 fclose(fid);
 chanMap = readNPY('channel_map.npy');
-dat = dat(chanMap+1,:);
-figure; imagesc(dat(:,1:30000))
+rawData = rawData(chanMap+1,:);
+% figure; 
+% imagesc(rawData(:,:));
+figure;
+hold on
+for i = 1:5
+    subplot(5, 1, i);
+    plot(1705000:1800000, rawData(i, 1705000:1800000));
+    set(gca, 'xticklabel', [])
+end
+hold off
