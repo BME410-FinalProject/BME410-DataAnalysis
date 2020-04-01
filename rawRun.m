cd33016353057104dce05b00e8e435264510b569
Lindsay Karaba <karaba24@gmail.com>
2020-04-01 00:59:17 -0400

    Update rawRun.m
    Should be fully commented and good to submit! Plots are labeled as well.

--- rawRun.m
+++ rawRun.m
@@ -1,19 +1,48 @@
+%{
+BME 410 Data Analysis Project
+Code Submission 4.2.20
+Noah Smith, Charlie Martin, Lindsay Karaba
+
+This program extracts the raw voltage data recorded from neuropixel
+probes and forms two plots. The first shows voltage (represented by color)
+across 72 channels over the timespan. The second extracts the voltage trace
+over time for selected channels of the probe. 
+%}
+%% Ensure numpy path is established
+%note that addpath path must be changed for specific computer
 addpath('D:\Documents\Word\College\Junior Year\Semester 2\BME 410\npy-matlab-master')
 savepath
 
+%% Read in raw data file
+%note that fn path must be changed for specific computer
 fn = 'D:\Documents\Word\College\Junior Year\Semester 2\BME 410\rawDataSample.bin';
 fid = fopen(fn, 'r');
 rawData = fread(fid, [385 Inf], '*int16');
 fclose(fid);
 chanMap = readNPY('channel_map.npy');
 rawData = rawData(chanMap+1,:);
-% figure; 
-% imagesc(rawData(:,:));
+
+%% Plot color map of data
+figure; 
+imagesc(rawData(:,:));
+colorbar;
+title('Raw voltage data for 72 channels over time');
+xlabel('Time (ms)');
+ylabel('Channel number');
+
+%% Plot set of traces
 figure;
 hold on
-for i = 1:5
+for i = 1:5 %arbitrary number of traces, chose 5 for simplicity (max of 72)
     subplot(5, 1, i);
-    plot(1705000:1800000, rawData(i, 1705000:1800000));
-    set(gca, 'xticklabel', [])
+    plot(1705000:1800000, rawData(i, 1705000:1800000)); %plot small section of time
+    if i < 5
+        set(gca, 'xticklabel', []) %remove xticks for all but bottom plot
+    end
+    if (i == 1)
+        title('Raw voltage traces of channels 1-5 (descending) over time');
+    end
 end
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
 hold off
